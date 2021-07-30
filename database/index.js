db1e4915e3e324d0401f4fd97b702790a3589d17
ANELL1 <lizcuevas68@gmail.com>
2021-07-30 14:34:36 -0500

    update

--- database/index.js
+++ database/index.js
@@ -1,15 +1,30 @@
-var mysql = require ('mysql')
+var mysql = require('mysql')
+var con = mysql.createPool({
+    database:"heroku_f5b964fd01c6149",
+    host:"us-cdbr-east-04.cleardb.com",
+    user:"b0fa8c8ce35bfc",
+    password:"c952ba31"
+},
+    console.log("conectado a la BD")
+) 
 
-var coneccion= mysql.createConnection({
-    database:'directorioalfa',
-    host:'localhost',
-    user:'root',
-    password:'user'
-})
+module.exports =  con;
 
-coneccion.connect(function(error){
-   console.log('este es el error',error)
-   if(error) throw error   
-   console.log('conectado a la base de datos')
-})
-module.exports=coneccion
+
+// var mysql = require ('mysql')
+
+// var coneccion= mysql.createConnection({
+//     database:'directorioalfa',
+//     host:'localhost',
+//     user:'root',
+//     password:'user'
+// })
+
+// coneccion.connect(function(error){
+//    console.log('este es el error',error)
+//    if(error) throw error   
+//    console.log('conectado a la base de datos')
+// })
+// module.exports=coneccion
+
+
