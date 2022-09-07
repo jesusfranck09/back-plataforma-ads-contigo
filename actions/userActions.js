43a9a281675832c03eefa5b47787d84a23721c6e
jesusfranck09 <d93409@gmail.com>
2022-09-07 12:19:08 -0500

    update

--- actions/userActions.js
+++ actions/userActions.js
@@ -1121,7 +1121,7 @@ const GetPolizasById = ( data)  => {
 }
 const EndSupport = ( data)  => {
     return new Promise((resolve,reject)=>{
-        // client.query(`update soporte set status = '${data[3]}', fechaFinalizacion = '${data[4]}' where id_soporte = '${data[0]}'`)
+        client.query(`update soporte set status = '${data[3]}', fechaFinalizacion = '${data[4]}' where id_soporte = '${data[0]}'`)
         client.query(`select * from contacto where id_contacto = '${data[1]}'`, function(err,results,fields){
             var string = JSON.stringify(results);
             var resultados = JSON.parse(string); 
@@ -1139,19 +1139,15 @@ const EndSupport = ( data)  => {
                 });
                 const mailOptions = {
                     from: 'ventas@adscontigo.com', // sender address
-                // to: `${resultados[0].correo1},jesus.francisco@ads.com.mx,miriam.quiroz@ads.com.mx `,
+                to: `${resultados[0].correo1},jesus.francisco@ads.com.mx,miriam.quiroz@ads.com.mx `,
                 // No olvide calificar la calidad de nuestro servicio por medio de la encuesta de satisfaccion mediante el siguiente enlace 
                 // <br/>
                 // <br/><br/>
                 // <p> https://plataforma.adscontigo.com/qualitySurvey:&${data[0]} </p><br/>
-                to: `jesus.francisco@ads.com.mx`, 
-                subject: 'Gracias por su interés en Alfa y Diseño de Sistemas', // Subject line
-                text: 'Seguimiento de solicitud de Soporte técnico',
-                html: `<p>Alfa y Diseño de Sistemas, es un Distribuidor Asociado Master de CONTPAQi®
-                    que ha recibido el reconocimiento como el Primer Lugar en Ventas por 16 Años consecutivos en la
-                    Ciudad de México.
-                    <br/>
-                    <br/>
+                // to: `d93409@gmail.com`, 
+                subject: 'Información de seguimiento de solicitudes de soporte', // Subject line
+                text: 'Proceso de soporte técnico',
+                html: `<p>
                         Solicitud de soporte mediante la plataforma ADS Contigo apartado Clientes con el folio <strong> ${data[5]} </strong> de la empresa ${data[9]}, RFC ${data[10]}, <br/>
                     <br/>
                         Estimado cliente, le notificamos que ha concluido de forma satisfactoria el proceso de soporte solicitado mediante los siguientes datos.
